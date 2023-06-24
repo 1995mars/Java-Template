80b792dc5e82c67ebaa5614e2329119c39d7d795
Mars <51732119+1995mars@users.noreply.github.com>
2023-06-24 18:33:10 +0700

    sonaqube

--- 1995mars_annotation/src/main/java/com/mars/template_annotation/exception/GlobalExceptionHandler.java
+++ 1995mars_annotation/src/main/java/com/mars/template_annotation/exception/GlobalExceptionHandler.java
@@ -6,18 +6,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-//    @ExceptionHandler({ Exception.class, })  // Có thể bắt nhiều loại exception
-//    public ResponseEntity<String> handleExceptionA(Exception e) {
-//        return ResponseEntity.status(432).body(e.getMessage());
-//    }
-//
-//    // Có thêm các @ExceptionHandler khác...
-
-    // Nên bắt cả Exception.class
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnwantedException(Exception e) {
         // Log lỗi ra và ẩn đi message thực sự (xem phần 3.2)
         e.printStackTrace();  // Thực tế người ta dùng logger
         return ResponseEntity.status(500).body("Unknow error");
     }
+
 }
